09adbc994c3b30bed74c0e94b0280fa1d47f176c
Laytan Laats <Laytanlaats@hotmail.com>
2021-03-22 20:11:15 +0100

    dont minfy js on watch mode

--- rollup.config.js
+++ rollup.config.js
@@ -2,25 +2,30 @@ import { babel } from '@rollup/plugin-babel';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
 
+const plugins = [
+  nodeResolve({
+    browser: true,
+  }),
+  babel({
+    babelHelpers: 'bundled',
+    presets: ["@babel/preset-env"],
+    plugins: [
+      "@babel/plugin-proposal-class-properties",
+      "@babel/plugin-proposal-private-methods",
+      "@babel/plugin-transform-classes"
+    ],
+  }),
+];
+
+if (process.env.NODE_ENV === 'production') {
+  plugins.push(terser());
+}
+
 export default {
   input: 'src/js/main.js',
   output: {
     file: 'src/js/bundle.js',
     format: 'iife',
   },
-  plugins: [
-    nodeResolve({
-      browser: true,
-    }),
-    babel({
-      babelHelpers: 'bundled',
-      presets: ["@babel/preset-env"],
-      plugins: [
-        "@babel/plugin-proposal-class-properties",
-        "@babel/plugin-proposal-private-methods",
-        "@babel/plugin-transform-classes"
-      ],
-    }),
-    terser(),
-  ],
+  plugins,
 };
